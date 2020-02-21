cefaef3dd04c84ccc5828bbad0a4b3d4c7040f94
Viljami Linna <viljami.linna@gmail.com>
2020-02-21 11:26:17 +0100

    lab2: Implement triangle normal vector calculaiton.

--- lab2/Code/shapes/triangle.cpp
+++ lab2/Code/shapes/triangle.cpp
@@ -14,6 +14,11 @@ Triangle::Triangle(Point const &v0,
     v2(v2),
     N()
 {
+
     // Calculate the surface normal here and store it in the N,
+    // plane direction vectors
+    Vector u = v1-v0;
+    Vector v = v2-v0;
+    N = u.cross(v);
     // which is declared in the header. It can then be used in the intersect function.
 }

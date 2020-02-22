03b9f91b99f2044e074f4fbdda9001b512c3aa62
Viljami Linna <viljami.linna@gmail.com>
2020-02-22 16:19:51 +0100

    lab2: Implement Quad constructor.

--- lab2/Code/shapes/quad.cpp
+++ lab2/Code/shapes/quad.cpp
@@ -13,7 +13,6 @@ Hit Quad::intersect(Ray const &ray)
 Quad::Quad(Point const &v0,
            Point const &v1,
            Point const &v2,
-           Point const &v3)
+           Point const &v3) : _triangles(std::pair<Triangle, Triangle>(Triangle(v0, v1, v2),Triangle(v1, v2, v3)))
 {
-    // Store and/or process the points defining the quad here.
 }

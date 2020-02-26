9c58d39877a584f74bb5c652fb9f6a75403fdc7c
Viljami Linna <viljami.linna@gmail.com>
2020-02-26 10:06:44 +0100

    lab3: Add normals to vertex_3d class.

--- lab3/Code/vertex_3d.h
+++ lab3/Code/vertex_3d.h
@@ -1,6 +1,7 @@
 #ifndef VERTEX_3D_H
 #define VERTEX_3D_H
 
+#include <limits>
 
 struct vertex_3d {
     float x;
@@ -9,6 +10,10 @@ struct vertex_3d {
     float R;
     float G;
     float B;
+    float normalX = std::numeric_limits<float>::min();
+    float normalY = std::numeric_limits<float>::min();
+    float normalZ = std::numeric_limits<float>::min();
+
 
 };
 #endif // VERTEX_3D_H

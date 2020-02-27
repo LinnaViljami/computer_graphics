ae1ff03521d8b6d2fc321b874d50f6d668a55439
Jurrian <jurrdb@gmail.com>
2020-02-27 12:47:09 +0100

    gouraud and phong fragshaders divide colour by 2 and 3 resp. to be able to differentiate between them (selecting shaders 2.)

--- lab3/Code/shaders/fragshader_gouraud.glsl
+++ lab3/Code/shaders/fragshader_gouraud.glsl
@@ -18,6 +18,6 @@ void main()
 {
     vec3 normalizedVertNormal = normalize(vertNormal);
     vec3 colorMapping = vec3(0.5,0.5,0.5);
-    vec3 mappedColors = colorMapping*normalizedVertNormal + colorMapping;
+    vec3 mappedColors = (colorMapping*normalizedVertNormal + colorMapping) / 2;
     fColor = vec4(mappedColors, 1.0);
 }

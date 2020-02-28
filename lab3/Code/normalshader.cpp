8b0bebd400a595f84cf3310ec6af65d986c85ef5
Viljami Linna <viljami.linna@gmail.com>
2020-02-28 15:09:58 +0100

    lab3: Implement normalShader.

--- lab3/Code/normalshader.cpp
+++ lab3/Code/normalshader.cpp
@@ -4,3 +4,27 @@ NormalShader::NormalShader()
 {
 
 }
+
+ShadingMode NormalShader::type()
+{
+    return ShadingMode::NORMAL;
+}
+
+void NormalShader::createShaderPrograms()
+{
+    shaderProgram.addShaderFromSourceFile(QOpenGLShader::Vertex,
+                                           ":/shaders/vertshader_normal.glsl");
+    shaderProgram.addShaderFromSourceFile(QOpenGLShader::Fragment,
+                                           ":/shaders/fragshader_normal.glsl");
+    shaderProgram.link();
+
+}
+
+void NormalShader::setUniformLocations()
+{
+    transformationUniformLocation = shaderProgram.uniformLocation("transformation");
+    projectionUniformLocation = shaderProgram.uniformLocation("projection");
+    normalTransformationUniformLocation = shaderProgram.uniformLocation("normalTransformation");
+    materialUniformLocation = shaderProgram.uniformLocation("material");
+    lightPositionUniformLocation = shaderProgram.uniformLocation("lightPosition");
+}

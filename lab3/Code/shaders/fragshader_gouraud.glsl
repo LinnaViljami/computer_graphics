182c4a76baf1223827259b1c741c9f9d205d56b5
Viljami Linna <viljami.linna@gmail.com>
2020-03-06 09:01:05 +0100

    Implement gourad shader in glsl file.

--- lab3/Code/shaders/fragshader_gouraud.glsl
+++ lab3/Code/shaders/fragshader_gouraud.glsl
@@ -6,7 +6,6 @@
 // Specify the inputs to the fragment shader
 // These must have the same type and name!
 in vec3 vertColor;
-in vec2 textureCoords;
 // Specify the Uniforms of the fragment shaders
 // uniform vec3 lightPosition; // for example
 
@@ -14,7 +13,6 @@ in vec2 textureCoords;
 // Usually a vec4 describing a color (Red, Green, Blue, Alpha/Transparency)
 out vec4 fColor;
 
-uniform sampler2D textureUniform;
 
 void main()
 {
--- lab3/Code/shaders/vertshader_gouraud.glsl
+++ lab3/Code/shaders/vertshader_gouraud.glsl
@@ -17,7 +17,7 @@ uniform vec3 material;              // ka, kd, ks
 uniform vec3 materialColor;         // r, g, b
 uniform vec3 lightPosition;         // x, y, z
 uniform vec3 lightColor;            // r, g, b
-
+uniform sampler2D textureUniform;
 // x, y, z
 vec3 viewAngle = vec3(0, 0, -1);
 
@@ -26,7 +26,6 @@ uniform float phongExponent;
 
 // Specify the output of the vertex stage
 out vec3 vertColor;
-out vec2 textureCoords;
 
 vec3 calculateNormalizedVector(vec3 from, vec3 to) {
     vec3 result = to - from;
@@ -68,14 +67,14 @@ vec3 calculateSpecularComponent(vec3 orientedNormal, vec3 lightVector) {
     return specularColor;
 }
 
-vec3 getPhongColor(vec3 orientedNormal) {
+vec3 getPhongColor(vec3 orientedNormal, vec3 textureColor) {
     vec3 lightVector = calculateNormalizedVector(vertCoordinates_in, lightPosition);
 
-    vec3 ambient = calculateAmbientComponent(materialColor);
-    vec3 diffuse = calculateDiffuseColor(orientedNormal, lightVector, materialColor);;
+    vec3 ambient = calculateAmbientComponent(textureColor);
+    vec3 diffuse = calculateDiffuseColor(orientedNormal, lightVector, textureColor);;
     vec3 specular = calculateSpecularComponent(orientedNormal, lightVector);
 
-    vec3 color = (ambient+diffuse+specular) * materialColor;
+    vec3 color = (ambient+diffuse+specular) * textureColor;
     return color;
 }
 
@@ -83,8 +82,8 @@ void main()
 {
     // gl_Position is the output (a vec4) of the vertex shader
     gl_Position = projection * transformation * vec4(vertCoordinates_in, 1.0);
-    textureCoords = textureCoords_in;
+    vec4 textureColor = texture(textureUniform, textureCoords_in);
     vec3 orientedNormal = normalTransformation * vertNormal_in;
 
-    vertColor = getPhongColor(normalize(orientedNormal));
+    vertColor = getPhongColor(normalize(orientedNormal), textureColor.xyz);
 }
